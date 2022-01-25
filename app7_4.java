7f56e2d16b80e76c71b84d0a0529c3957cde48dc
guantou <mole30608@gmail.com>
2022-01-26 03:21:00 +0800

    Update app7_4.java

--- app7_4.java
+++ app7_4.java
@@ -1,6 +1,5 @@
 /*
 filename:app7_4
-author:40843120 wu,guan-ting
 feature:There was a year of insufficient power supply in New Zealand. Therefore, the Biyang district was divided into 17 districts in turn. In order to be able to take turns in a fair manner, the decision-maker chose a random number of 5. The zone starts, and the next is zone 6, that is, the power outage is carried out with 5 as the interval, then the 11th and 16th zones, because there are only 17 zones, so start from the beginning, and there is no need to power out again if it has stopped . So I counted 5 times (17, 2, 3, 4, 5), so it was the turn of the 5th district to lose power. The sequence of the entire power outage is:
                    1 6 11 16 5 12 2 9 17 10 4 15 14 3 8 13 7
 However, this decision maker felt that using the number 5 seemed unfair, so the public could choose the number n between 2 and 10. Write a program that allows the user to enter the number n, and let the program sequence the area code of the power failure
