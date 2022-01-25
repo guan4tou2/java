4a379ff827fe70bc629a37a12b94a9cce990f1df
guantou <mole30608@gmail.com>
2022-01-26 03:16:37 +0800

    Update app3_1.java

--- app3_1.java
+++ app3_1.java
@@ -1,15 +1,14 @@
 /*
 filename:app3_1
 feature:use unicode show chinese name
-author:40843120 wu,guan-ting
 data:2020/10/05
 */
 
 public class app3_1{
     public static void main(String args[]){
-	char ch1='吳';//21555
-	char ch2='冠';//20896
-	char ch3='廷';//24311
-    System.out.println("吳="+(int)ch1+" 冠="+(int)ch2+" 廷="+(int)ch3);
+	char ch1='';//
+	char ch2='';//
+	char ch3='';//
+    System.out.println("="+(int)ch1+" ="+(int)ch2+" ="+(int)ch3);
     }
 }
